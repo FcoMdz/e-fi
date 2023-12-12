9d691ee1b0225f7fd02df5b53abf52eda2fc991c
Gerardo Femat Delgado <111470468+Gerardo1209@users.noreply.github.com>
2023-12-12 11:26:40 -0600

    Actualizar terminado

--- app/src/main/java/com/example/e_fi/ActualizacionActivity.kt
+++ app/src/main/java/com/example/e_fi/ActualizacionActivity.kt
@@ -5,15 +5,18 @@ import android.app.DatePickerDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.OpenableColumns
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.parse.ParseException
 import com.parse.ParseFile
 import com.parse.ParseObject
+import com.parse.ParseQuery
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 import java.text.SimpleDateFormat
@@ -21,25 +24,36 @@ import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
-class RegistroActivity : AppCompatActivity() {
+class ActualizacionActivity : AppCompatActivity() {
 
     private var selectedKeyUri: Uri? = null
     private var selectedCerUri: Uri? = null
     private lateinit var subirKeyButton: Button
     private lateinit var subirCerButton: Button
+    private lateinit var id:String
+    private lateinit var cliente:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.agregar_layout)
+        setContentView(R.layout.update_layout)
 
-        val nombreEditText: EditText = findViewById(R.id.nombreEditText)
-        val contrasenaEditText: EditText = findViewById(R.id.contrasenaEditText)
-        val correoEditText: EditText = findViewById(R.id.correoEditText)
-        val rfcEditText: EditText = findViewById(R.id.rfcEditText)
-        val fechaEditText: Button = findViewById(R.id.fechaEditButton)
+        val intent = intent
+        if(intent != null){
+            this.id = intent.getStringExtra("id").toString()
+            this.cliente = intent.getStringExtra("cliente").toString()
+        }
+
+        val regresarButton: ImageButton = findViewById(R.id.regresar)
+        val nuevoNombreEditText: EditText = findViewById(R.id.nuevoNombreEditText)
+        val nuevaFechaEditText: Button = findViewById(R.id.fechaEditButton)
         this.subirKeyButton = findViewById(R.id.subirKeyButton)
         this.subirCerButton = findViewById(R.id.subirCerButton)
-        val registrarButton: Button = findViewById(R.id.registrarButton)
-        val regresarButton: ImageButton = findViewById(R.id.regresar)
+        val actualizarButton: Button = findViewById(R.id.actualizarButton)
+        val titulo:TextView = findViewById(R.id.titulo)
+
+        if(this.cliente != null){
+            titulo.text = titulo.text.toString() + " " + this.cliente
+        }
 
         regresarButton.setOnClickListener {
             finish()
@@ -61,7 +75,7 @@ class RegistroActivity : AppCompatActivity() {
             intent.type = "application/*"
             startActivityForResult(intent, REQUEST_CER_PICK)
         }
-        fechaEditText.setOnClickListener {
+        nuevaFechaEditText.setOnClickListener {
             val calendar = Calendar.getInstance()
             val datePickerDialog = DatePickerDialog(
                 this,
@@ -71,7 +85,7 @@ class RegistroActivity : AppCompatActivity() {
                     fechaSeleccionada = selectedDate.time
                     val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
                     val formattedDate = dateFormat.format(selectedDate.time)
-                    fechaEditText.text = formattedDate
+                    nuevaFechaEditText.text = formattedDate
                 },
                 calendar.get(Calendar.YEAR),
                 calendar.get(Calendar.MONTH),
@@ -81,42 +95,56 @@ class RegistroActivity : AppCompatActivity() {
             datePickerDialog.show()
         }
 
-        registrarButton.setOnClickListener {
-            val nombre = nombreEditText.text.toString()
-            val contrasena = contrasenaEditText.text.toString()
-            val rfc = rfcEditText.text.toString()
-            val correo = correoEditText.text.toString()
-            // Validar que la imagen se haya cargado antes de proceder con el registro
+        actualizarButton.setOnClickListener {
+            val nuevoNombre = nuevoNombreEditText.text.toString()
+            val nuevaFecha = nuevaFechaEditText.text.toString()
+
             if (selectedKeyUri != null && selectedCerUri != null &&
-                nombre.isNotEmpty() && contrasena.isNotEmpty() && rfc.isNotEmpty()
-                && fechaSeleccionada != null && correo.isNotEmpty()) {
-                val nfecha = Date()
-                val efirma = ParseObject("Firma_Electronica")
-                efirma.put("contrasena", contrasena)
-                efirma.put("contacto", correo)
-                efirma.put("cliente", nombre)
-                efirma.put("fecha_emision", fechaSeleccionada!!)
-                efirma.put("RFC", rfc)
-                val certificado = convertTextFileToBytes(selectedCerUri!!)
-                val cert = ParseFile(subirCerButton.text.toString(), certificado)
-                efirma.put("cer", cert)
-                val llave = convertTextFileToBytes(selectedKeyUri!!)
-                val key = ParseFile(subirKeyButton.text.toString(), llave)
-                efirma.put("key", key)
+                nuevoNombre.isNotEmpty() && nuevaFecha.isNotEmpty() &&
+                fechaSeleccionada != null && this.id != null
+            ) {
 
-                efirma.saveInBackground {e ->
-                    if(e == null){
-                        //Exito al subir los datos
-                        Toast.makeText(this@RegistroActivity, "Registro exitoso", Toast.LENGTH_SHORT).show()
-                    }else{
-                        Toast.makeText(this@RegistroActivity, "Ha fallado el registro",
-                            Toast.LENGTH_SHORT).show()
+                // Obtener el objeto Parse que deseas actualizar (puedes pasar esto como extra desde la actividad anterior)
+                val objectId = "ID_DEL_OBJETO_A_ACTUALIZAR" // Reemplaza con el ID correcto
+                val query = ParseQuery.getQuery<ParseObject>("Firma_Electronica")
+                query.whereEqualTo("objectId", this.id)
+                try{
+                    val objetoAActualizar =
+                        query.first
+                    val certificado = convertTextFileToBytes(selectedCerUri!!)
+                    val cert = ParseFile(subirCerButton.text.toString(), certificado)
+                    val llave = convertTextFileToBytes(selectedKeyUri!!)
+                    val key = ParseFile(subirKeyButton.text.toString(), llave)
+                    objetoAActualizar?.apply {
+                        put("nombre", nuevoNombre)
+                        put("fecha_emision", fechaSeleccionada!!)
+                        put("cer", cert)
+                        put("key", key)
                     }
+                    // Guardar los cambios
+                    objetoAActualizar.saveInBackground { e ->
+                        if (e == null) {
+                            // Éxito al guardar en Parse
+                            Toast.makeText(
+                                this@ActualizacionActivity,
+                                "Actualización exitosa",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            // Error al guardar en Parse
+                            Toast.makeText(
+                                this@ActualizacionActivity,
+                                "Error al actualizar: $e",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }catch (e: ParseException){
+                    e.printStackTrace()
                 }
-
             } else {
-                // Al menos un campo está vacío o la imagen no se cargó
-                Toast.makeText(this@RegistroActivity, "Completa todos los campos y carga la firma electrónica", Toast.LENGTH_SHORT).show()
+                // Al menos un campo está vacío o la imagen o el archivo de texto no se cargaron
+                Toast.makeText(this@ActualizacionActivity, "Completa todos los campos y carga la firma electrónica", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -164,12 +192,13 @@ class RegistroActivity : AppCompatActivity() {
         }
         return result
     }
+
     private fun convertTextFileToBytes(textFileUri: Uri): ByteArray {
         val inputStream: InputStream? = contentResolver.openInputStream(textFileUri)
         val buffer = ByteArrayOutputStream()
 
         inputStream?.use { input ->
-            val tempBuffer = ByteArray(4096)
+            val tempBuffer = ByteArray(1024)
             var bytesRead: Int
             while (input.read(tempBuffer).also { bytesRead = it } != -1) {
                 buffer.write(tempBuffer, 0, bytesRead)
@@ -178,6 +207,7 @@ class RegistroActivity : AppCompatActivity() {
 
         return buffer.toByteArray()
     }
+
     companion object {
         private const val REQUEST_CER_PICK = 1
         private const val REQUEST_KEY_PICK = 2
